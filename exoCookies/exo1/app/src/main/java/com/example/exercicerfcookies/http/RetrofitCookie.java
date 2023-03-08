a46a6c443987084a9708ff52061b5301fca99d13
Rayane <2111752@cegepmontpetit.ca>
2023-03-07 20:10:15 -0500

    ajout labo retrofitCookie

---
+++ exoCookies/exo1/app/src/main/java/com/example/exercicerfcookies/http/RetrofitCookie.java
@@ -0,0 +1,19 @@
+package com.example.complexe.http;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class RetrofitUtil {
+
+    public static Service get(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://4n6.azurewebsites.net/")
+                .build();
+
+        Service service = retrofit.create(Service.class);
+        return service;
+    }
+}
---
+++ exoCookies/exo1/app/src/main/java/com/example/exercicerfcookies/http/cookieService.java
@@ -0,0 +1,2 @@
+package com.example.exercicerfcookies.http;public interface cookieService {
+}
